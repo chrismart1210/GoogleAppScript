2721da4951132eaaf9e90e3f95a9daa7eccef9fc
Chris <108366775+chrismart1210@users.noreply.github.com>
2023-09-14 13:02:20 -0300

    Robot version 1
    Tenemos el robot de ImportRange de mas facil uso.
    Directamente es un ImportRange en version duro ...

---
+++ BackUper.js
@@ -0,0 +1,18 @@
+function Sheet__BackUper()
+{
+  // INPUT's //
+  var Sheet_origen = Sheet_Origen;
+  var Sheet_destino = Sheet_Destino;
+  
+  // Obtener los valores de todo el rango de datos
+    var data = Sheet_origen.getDataRange().getValues();
+
+  // Pegar los valores en la hoja de destino
+    Sheet_destino.getRange(1, 1, data.length, data[0].length).setValues(data);
+
+  // Agregar estampa de tiempo en el formato deseado
+    var fechaActual = new Date();
+    var estampaTiempo = Utilities.formatDate(fechaActual, "GMT-3", "dd/MM - HH:mm");
+    // OUTPUT
+      Sheet_destino.getRange(1, 1).setValue(estampaTiempo);
+}
